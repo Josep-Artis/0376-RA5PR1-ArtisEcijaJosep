bf94a3028eaba1247f0ffa7959cb5b876d314f26
JosepArtis <jartis25@ilb.cat>
2026-03-25 12:25:47 +0100

    primer-commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>TAULA DE MULTRIPLICAR</h1>
+    <p>Introducimos un numero</p>
+    <?php
+    $num = 8;
+    for ($i=1; $i <= 10; $i++) { 
+        echo "$num x $i = " . ($num * $i) . "<br>";
+    }
+</body>
+</html>
