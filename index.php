7208d06b0325ae9be1852bbeeed9804075db808d
JosepArtis <jartis25@ilb.cat>
2026-04-07 12:31:15 +0200

    una vez tenemos la parte funcional de la tabla solo quedara hacer el estilo de la tabla

--- index.php
+++ index.php
@@ -6,14 +6,32 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>TAULA DE MULTRIPLICAR</h1>
-    <p>Introducimos un numero</p>
+    <h1>TAULA DE MULTIPLICAR</h1>
+
     <?php
-    $num = 8;
-    $minimo = 0;
-    $maximo = 10;
-    for ($i = $minimo; $i <= $maximo; $i++) { 
-        echo "$num x $i = " . ($num * $i) . "<br>";
-    }?>
+    // variable de número
+    $num = 8; 
+
+    //Validación entre 1 y 12
+    if ($num < 1 || $num > 12) {
+        echo "<p class='error'>Error: El número ha de ser entre 1 i 12.</p>";
+    } else {
+        echo "<table>";
+        // Tu bucle, pero generando filas (tr) y celdas (td)
+        for ($i = 1; $i <= 10; $i++) { 
+            $clase = "";
+            if ($i % 2 == 0) {
+                $clase = "class='gris'";
+            }
+
+            echo "<tr $clase>";
+                echo "<td>$num x $i</td>";
+                echo "<td>" . ($num * $i) . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+    ?>
+
 </body>
 </html>
